5d740ed4fe2d64427638107cdab73d909b278be4
unknownperson2000 <82877740+unknownperson2000@users.noreply.github.com>
2024-08-03 17:13:05 +0300

    comment

---
+++ project/script.js
@@ -0,0 +1,776 @@
+let products ={
+    data: [
+      {
+        productName: "RASPBERRY PI 4 MODEL B (8GB)",
+        category: "b",
+        price: "89.9",
+       image:
+       "img/n.jpg"      
+  
+      },
+      {
+        productName: "WIFI SMART CIRCUIT BREAKER TIMER",
+        category: "b",
+        price: "33",
+        image:
+        "img/b2.jpg"    
+        },
+      {
+        productName: "NFC SMART RING BLACK 11",
+        category: "b",
+        price: "7",
+        image:
+        "img/b3.jpg"  
+                },
+      {
+        productName: "LTE ANTENNA FOR 3G",
+        category: "b",
+        price: "42",
+        image:
+        "img/b4.jpg" 
+           },
+      {
+        productName: "NFC Smart Ring Red 13",
+        category: "b",
+        price: "5",
+        image:
+        "img/b5.jpg" 
+            },
+      {
+        productName: "Egg Incubator System",
+        category: "b",
+        price: "36",
+        image:
+        "img/b6.jpg" 
+         },
+      {
+        productName: "Lab Dosing Pump Mini Self",
+        category: "b",
+        price: "9",
+        image:
+        "img/b7.jpg"
+         },
+      {
+        productName: "DS5160 270 DIGITAL SERVO ",
+        category: "b",
+        price: "44",
+        image:
+          "img/b8.jpg"
+      },
+      {
+        productName: "DIY ELECTRIC STEPPING ELLIPTICAL MACHINE",
+        category: "b",
+        price: "8",
+        image:
+        "img/b9.jpg"      },
+      {
+        productName: "Raspberry Pi Zero W 2",
+        category: "b",
+        price: "29",
+        image:
+        "img/b10.jpg"      },
+      {
+        productName: "Mz80 Infrared Sensor",
+        category: "s",
+        price: "13",
+        image:
+           "img/s1.jpg"      },
+      {
+        productName: "ARDUINO NANO BLE SENSE REV2",
+        category: "s",
+        price: "47",
+        image:
+      "img/s2.jpg"      },
+      {
+        productName: "Adjustable Aluminum sensor",
+        category: "s",
+        price: "5.9",
+        image:
+        "img/s3.jpg"         
+      },
+      {
+        productName: "XLINE 16 Sensor",
+        category: "s",
+        price: "27.5",
+        image:
+          "img/s4.jpg"
+         ,
+      },
+      {
+        productName: "Wing Arduino Nano Robot",
+        category: "s",
+        price: "34",
+        image:
+          "img/s5.jpg"
+         ,
+      },
+      {
+        productName: "QTR-8RC Line Sensor",
+        category: "s",
+        price: "15",
+        image:
+           "img/s6.jpg"         ,
+      },
+      {
+        productName: "Micro Line Sensor ML1",
+        category: "s",
+        price: "2.75",
+        image:
+          "img/s7.jpg"         ,
+      },
+      {
+        productName: "MGLINE 5 Way Line Sensor",
+        category: "s",
+        price: "10",
+        image:
+           "img/s8.jpg"         ,
+      },
+      {
+        productName: "Premium Watch",
+        category: "s",
+        price: "90",
+        image:
+         "img/s9.jpg"         ,
+      },
+      {
+        productName: "RZ60S Digital Infrared sensor",
+        category: "s",
+        price: "15",
+        image:
+        "img/s9.jpg"
+         ,
+      },
+      {
+        productName: "Arduino Opta WiFi PLC",
+        category: "ar",
+        price: "239",
+        image:
+       "img/a1.jpg"         ,
+      },
+      {
+        productName: "Arduino UNO R4 WiFi",
+        category: "ar",
+        price: "35",
+        image:
+          "img/a2.jpg"         ,
+      },
+      {
+        productName: "Arduino UNO R4 Minima",
+        category: "ar",
+        price: "26",
+        image:
+         "img/a3.jpg"         ,
+      },
+      {
+        productName: "Wing Arduino Nano Robot",
+        category: "ar",
+        price: "34",
+        image:
+        "img/a4.jpg"
+         ,
+      },
+      {
+        productName: "Arduino UNO R4 Minima copy",
+        category: "ar",
+        price: "15",
+        image:
+        "img/a5.jpg"         ,
+      },
+      {
+        productName: "Premium Watch",
+        category: "ar",
+        price: "90",
+        image:
+        "img/a6.jpg"
+         ,
+      },
+      {
+        productName: "Wi-Fi R3 ATmega328P ",
+        category: "ar",
+        price: "12",
+        image:
+          "img/a7.jpg"         ,
+      },
+      {
+        productName: "Premium Watch",
+        category: "ar",
+        price: "90",
+        image:
+        "img/a8.jpg"
+         ,
+      },
+      {
+        productName: "1.64FT Mini USB Cable",
+        category: "ar",
+        price: "1",
+        image:
+          "img/a9.jpg"         ,
+      },
+      {
+        productName: "Power Bank Supply",
+        category: "ar",
+        price: "12",
+        image:
+           "img/a10.jpg"         ,
+      },
+      {
+        productName: "Raspberry Pi 5 8GB",
+        category: "r",
+        price: "105",
+        image:
+            "img/r2.jpg"         ,
+      },
+      {
+        productName: "Raspberry Pi 5 4GB",
+        category: "r",
+        price: "87",
+        image:
+          "img/r2.jpg"         ,
+      },
+      {
+        productName: "Raspberry Pi 4 Model B ",
+        category: "r",
+        price: "99",
+        image:
+        "img/r3.jpg"         ,
+      },
+      {
+        productName: "RASPBERRY PI 4 MODEL B 8GB KIT",
+        category: "r",
+        price: "129",
+        image:
+         "img/r3.jpg"         ,
+      },
+      {
+        productName: "RASPBERRY PI 4 MODEL B (8GB) KIT",
+        category: "r",
+        price: "130",
+        image:
+        "img/r5.jpg"         ,
+      },
+      {
+        productName: "RASPBERRY PI 4 MODEL B (8GB) KIT",
+        category: "r",
+        price: "130",
+        image:
+           "img/r6.jpg"         ,
+      },
+      {
+        productName: "METAL CASE FOR RASPBERRY PI 5",
+        category: "r",
+        price: "12",
+        image:
+            "img/r7.jpg"         ,
+      },
+      {
+        productName: "Raspberry Pi 5 Case",
+        category: "r",
+        price: "6",
+        image:
+           "img/r8.jpg"         ,
+      },
+      {
+        productName: "RASPBERRY PI 5 HEAT SINK COPPER ",
+        category: "r",
+        price: "4.5",
+        image:
+          "img/r9.jpg"         ,
+      },
+      {
+        productName: "Raspberry Pi 3 Model",
+        category: "r",
+        price: "25",
+        image:
+        "img/r10.jpg"         ,
+      },
+      {
+        productName: "PNema34 12.2Nm 6A ",
+        category: "m",
+        price: "160",
+        image:
+          "img/m1.jpg"         ,
+      },
+      {
+        productName: "TAKANAWA 555 METAL GEAR MOTOR 24V 210RPM",
+        category: "m",
+        price: "18",
+        image:
+         "img/m2.jpg"         ,
+      },
+      {
+        productName: "TB6560 Upgrade Version",
+        category: "m",
+        price: "9",
+        image:
+         "img/m3.jpg"         ,
+      },
+      {
+        productName: "SK16UU LINEAR RAIL Shaft",
+        category: "m",
+        price: "3.85",
+        image:
+            "img/m4.jpg"         ,
+      },
+      {
+        productName: "Wheel Side Gear",
+        category: "m",
+        price: "33",
+        image:
+          "img/m5.jpg"      },
+      {
+        productName: "FingerTech 'Viper' Baseplate",
+        category: "m",
+        price: "39",
+        image:
+        "img/m6.jpg"
+         ,
+      },
+      {
+        productName: "FingerTech FT1806",
+        category: "m",
+        price: "28",
+        image:
+           "img/m7.jpg"         ,
+      },
+      {
+        productName: "FingerTech tinyESC v2",
+        category: "m",
+        price: "35",
+        image:
+         "img/m8.jpg"      },
+      {
+        productName: "FingerTech Foam",
+        category: "m",
+        price: "5",
+        image:
+         "img/m9.jpg"         ,
+      },
+      {
+        productName: "FingerTech Silver Spark",
+        category: "m",
+        price: "25",
+        image:
+      "img/m10.jpg"         ,
+      },
+      {
+        productName: "WIFI Smart Circuit",
+        category: "p",
+        price: "33",
+        image:
+        "img/p1.jpg"         ,
+      },
+      {
+        productName: "HOTA D6 Pro AC 200W",
+        category: "p",
+        price: "119",
+        image:
+           "img/p2.jpg"         ,
+      },
+      {
+        productName: "9V 650MWH RECHARGEABLE BATTERY",
+        category: "p",
+        price: "9",
+        image:
+         "img/p3.jpg"         ,
+      },
+      {
+        productName: "9v 5850mwh Rechargeable BATTERY",
+        category: "p",
+        price: "8",
+        image:
+           "img/p4.jpg",
+      },
+      {
+        productName: "3.7V 600mah LiPo ",
+        category: "p",
+        price: "3",
+        image:
+          "img/p5.jpg"         ,
+      },
+      {
+        productName: "3.7V 500mah LiPo ",
+        category: "p",
+        price: "3",
+        image:
+          "img/p6.jpg"         ,
+      },
+      {
+        productName: "LiPower LIPO BATTERY",
+        category: "p",
+        price: "55",
+        image:
+          "img/p7.jpg"         ,
+      },
+      {
+        productName: "LiPower LIPO BATTERY ",
+        category: "p",
+        price: "22",
+        image:
+         "img/p8.jpg"         ,
+      },
+      {
+        productName: "0.36″ DC 0-100 VOLTMETER",
+        category: "p",
+        price: "90",
+        image:
+        "img/p9.jpg"         ,
+      },
+      {
+        productName: "WAVESHARE 5INCH RESISTIVE TOUCH SCREEN LCD ",
+        category: "l",
+        price: "49",
+        image:
+           "img/l1.jpg"         ,
+      },
+      {
+        productName: "WAVESHARE 7inch",
+        category: "l",
+        price: "55",
+        image:
+         "img/l2.jpg"         ,
+      },
+      {
+        productName: "Premium Watch",
+        category: "l",
+        price: "0.25",
+        image:
+        "img/l3.jpg"         ,
+      },
+      {
+        productName: "10PCS SMD LED GREEN",
+        category: "l",
+        price: "0.25",
+        image:
+          "img/l4.jpg"         ,
+      },
+      {
+        productName: "10pcs 1206 SMD LED",
+        category: "l",
+        price: "0.1",
+        image:
+          "img/l5.jpg"         ,
+      },
+      {
+        productName: "Mini USB Led Light Lamp",
+        category: "l",
+        price: "1",
+        image:
+           "img/l6.jpg"         ,
+      },
+      {
+        productName: "7 Segment 2 Digit",
+        category: "l",
+        price: "1",
+        image:
+           "img/l7.jpg"         ,
+      },
+      {
+        productName: "DODO SMART RGB STRIP",
+        category: "l",
+        price: "25",
+        image:
+                "img/l8.jpg" ,
+      },
+      {
+        productName: "7 INCH 1024*600 RESISTIVE TOUCH SCREEN",
+        category: "l",
+        price: "80",
+        image:
+           "img/l9.jpg"      ,
+      },
+      {
+        productName: "7 Inch 1024x600 Disply",
+        category: "l",
+        price: "60",
+        image:
+          "img/l9.jpg" ,
+      },
+      {
+        productName: "10mm round water transparent",
+        category: "l",
+        price: "0.15",
+        image:
+           "img/l11.jpg"         ,
+      },
+      {
+        productName: "SIGNAL LAMP PANEL",
+        category: "l",
+        price: "1.5",
+        image:
+          "img/l12.jpg"         ,
+      },
+      {
+        productName: "7.0 inch Nextion ",
+        category: "l",
+        price: "127",
+        image:
+        "img/l13.jpg"         ,
+      },
+      {
+        productName: "P Channel MOSFET",
+        category: "e",
+        price: "0.75",
+        image:
+          "img/e1.jpg"         ,
+      },
+      {
+        productName: "Power MOSFET IRF530",
+        category: "e",
+        price: "0.75",
+        image:
+             "img/e2.jpg"         ,
+      },
+      {
+        productName: "10K ohm resistance",
+        category: "e",
+        price: "2",
+        image:
+            "img/e3.jpg"         ,
+      },
+      {
+        productName: "Solder Tip Iron Head",
+        category: "e",
+        price: "3.5",
+        image:
+             "img/e4.jpg"      },
+      {
+        productName: "MECHANIC DT30P5 DC",
+        category: "e",
+        price: "70",
+        image:
+           "img/e5.jpg"         ,
+      },
+      {
+        productName: "BC639 General Purpose",
+        category: "e",
+        price: "0.25",
+        image:
+           "img/e8.jpg"         ,
+      },
+      {
+        productName: "1 Position DIP Switch",
+        category: "e",
+        price: "0.25",
+        image:
+           "img/e8.jpg"         ,
+      },
+      {
+        productName: "UGREEN CR125 4-in-1 ",
+        category: "e",
+        price: "8.5",
+        image:
+        "img/e9.jpg"         ,
+      },
+      {
+        productName: "UGREEN CM304 2 in 1",
+        category: "e",
+        price: "6",
+        image:
+         "img/e10.jpg"         ,
+      },
+      {
+        productName: "Wedo 2.0 kids program",
+        category: "k",
+        price: "59",
+        image:
+         "img/k1.jpg"         ,
+      },
+
+      {
+        productName: "Plastic Storage Box",
+        category: "k",
+        price: "5",
+        image:
+           "img/k2.jpg"         ,
+      },
+      {
+        productName: "Plastic Storage Box",
+        category: "k",
+        price: "5",
+        image:
+         "img/k3.jpg"         ,
+      },
+      {
+        productName: "Plastic Storage Box",
+        category: "k",
+        price: "4",
+        image:
+             "img/k4.jpg"         ,
+      },
+      {
+        productName: "Keyestudio Mini",
+        category: "k",
+        price: "120",
+        image:
+          "img/k5.jpg"         ,
+      },
+      {
+        productName: "Double Layer Plastic",
+        category: "k",
+        price: "3",
+        image:
+         "img/k6.jpg"         ,
+      },
+      {
+        productName: "Electricity kit",
+        category: "k",
+        price: "23",
+        image:
+        "img/k7.jpg"         ,
+      },
+      {
+        productName: "ELECTRICAL CIRCUIT",
+        category: "k",
+        price: "1.76",
+        image:
+            "img/k8.jpg"         ,
+      },
+      {
+        productName: "DIY Electric Stepping",
+        category: "k",
+        price: "8",
+        image:
+          "img/k9.jpg"         ,
+      },
+      {
+        productName: "DIY Mini Solar Wooden",
+        category: "k",
+        price: "10",
+        image:
+          "img/k10.jpg"         ,
+      },
+     
+      
+     
+    ],
+  };
+
+
+  /* Creating Product Cards*/
+  for (let i of products.data) {
+    let card = document.createElement("div");
+    card.classList.add("card", i.category, "hide");
+    let container = document.createElement("div");
+    container.classList.add("container");
+  
+    card.innerHTML = `
+      <div class="image-container"><img src="${i.image}"></div>
+      <div><i class='bx bx-heart' id="heart-icon" ></i></div>
+      <div class="container">
+           <div>
+              <h5 class="product-name">${i.productName.toUpperCase()}</h5>
+              <h6>$ ${i.price}</h6>
+          </div>
+          <button class="cart-btn">Add to Cart</button>
+      </div>
+      `;
+  
+    card.appendChild(container);
+    document.getElementById("products").appendChild(card);
+  }
+
+
+  /* Filtering Products*/
+
+  function filterProduct(value) {
+    let buttons = document.querySelectorAll(".button-value");
+    buttons.forEach((button) => {
+      if (value.toUpperCase() == button.innerText.toUpperCase()) {
+        button.classList.add("active");
+      } else {
+        button.classList.remove("active");
+      }
+    });
+    let elements = document.querySelectorAll(".card");
+    elements.forEach((element) => {
+      if (value == "all") {
+        element.classList.remove("hide");
+      } else {
+        if (element.classList.contains(value)) {
+          element.classList.remove("hide");
+        } else {
+          element.classList.add("hide");
+        }
+      }
+    });
+  }
+ 
+  window.onload = () => {
+    filterProduct("all");
+};
+
+  /* Heart Icon Toggle*/
+  let heartBtns = document.querySelectorAll("#heart-icon");
+  heartBtns.forEach((icon) => {
+    icon.addEventListener("click", () => {
+      icon.classList.toggle("bxs-heart");
+    });
+  });
+
+
+//load content click me
+  const loadButton = document.getElementById('loadContent');
+  const contentContainer = document.getElementById('contentContainer');
+
+  loadButton.addEventListener('click', () => {
+     
+      setTimeout(() => {
+          contentContainer.innerHTML = '<p>TTU</p>';
+      }, 1000);
+  });
+  //load nav
+  document.addEventListener("DOMContentLoaded", function() {
+    const navLinks = document.querySelectorAll(".navbar a");
+    const pages = document.querySelectorAll(".page");
+
+    navLinks.forEach(link => {
+        link.addEventListener("click", function(event) {
+            event.preventDefault();
+            const target = this.getAttribute("data-target");
+            pages.forEach(page => {
+                page.style.display = "none";
+            });
+            document.getElementById(target).style.display = "block";
+        });
+    });
+});
+
+
+
+
+
+
+/*
+
+// JSON data representing button information
+const buttonData = [
+  { label: "Home", id: "homeButton" },
+  { label: "About", id: "aboutButton" },
+  { label: "Contact", id: "contactButton" },
+  { label: "Services", id: "servicesButton" }
+];
+
+// Function to create buttons from JSON data
+function createButtonsFromJSON(data) {
+  const container = document.getElementById("buttonContainer");
+
+  data.forEach(item => {
+      const button = document.createElement("button");
+      button.textContent = item.label;
+      button.className = "button";
+      button.id = item.id;
+
+      // Add event listener for button click
+      button.addEventListener("click", () => {
+          alert(`You clicked ${item.label}`);
+      });
+
+      container.appendChild(button);
+  });
+}
+
+// Execute function after the DOM is fully loaded
+document.addEventListener("DOMContentLoaded", () => {
+  createButtonsFromJSON(buttonData);
+});
+*/
