b29aa87c6f958a187572e59ba8688a3905470443
unknownperson2000 <82877740+unknownperson2000@users.noreply.github.com>
2024-08-03 17:09:26 +0300

    comment

--- project/js/n.js
+++ project/js/n.js
@@ -1,66 +1,26 @@
-const navFragment = document.createDocumentFragment();
-const mySec = document.querySelectorAll('section');
-const myNav = document.getElementById('navbar__list');
-const myLinks = document.getElementsByClassName("menu__link");
-const myButton = document.getElementById("integrate");
-const createNavBar = () => {
-    for (const section of mySec) {
-        const navItem = document.createElement('li');
-        const navItemLink = document.createElement('a');
-        navItemLink.classList.add('menu__link');
-        navItemLink.innerHTML = `${section.dataset.nav}`;
-        navItemLink.setAttribute("href", `#${section.id}`);
-        navItem.appendChild(navItemLink);
-        navFragment.appendChild(navItem);
-        myNav.append(navFragment);
-    };
-};
-const activeClass = () => {
-    for (const section of mySec) {
-        const rect = section.getBoundingClientRect();
-        const allNavLinks = document.querySelectorAll(`[href="#${section.id}"]`);
-        for (const link of allNavLinks) {
-            (rect.top >= 0 && rect.top < 500) ? (
-                `
-                ${section.classList.add("your-active-class")}
-                ${link.classList.add("menu__link__active")}
-                `
-            ) : (
+'use strict';
 
-                `
-                ${section.classList.remove("your-active-class")}
-                ${link.classList.remove("menu__link__active")}
-                `
-            );
-        };
-    };
-};
-const smoothScroll = () => {
-    for (const link of myLinks) {
-        link.addEventListener("click", e => {
-            e.preventDefault();
-            const sectionID = e.target.getAttribute("href").substring(1);
-            document.getElementById(`${sectionID}`).scrollIntoView({ behavior: "smooth" });
-        });
-    };
-};
-createNavBar();
-document.addEventListener('scroll', activeClass);
-smoothScroll();
-myButton.addEventListener("click", () => myNav.classList.toggle("toggle-show"));
 
-const scrollTopDiv = document.querySelector(".scroll-top");
-const scrollTopLink = document.querySelector(".scroll-top-link");
 
-window.onscroll = () => {
-    if (window.pageYOffset > 300) {
-        scrollTopDiv.style.display = "flex";
-    } else {
-        scrollTopDiv.style.display = "none";
-    };
-};
+/**
+ * Mobile navbar toggle
+ */
 
-scrollTopLink.addEventListener("click", e => {
-    e.preventDefault();
-    document.querySelector(".main__hero").scrollIntoView({ behavior: "smooth", block: "center" });
+const navbar = document.querySelector("[data-navbar]");
+const navToggler = document.querySelector("[data-nav-toggler]");
+
+navToggler.addEventListener("click", function () {
+  navbar.classList.toggle("active");
+});
+
+
+
+/**
+ * Header active
+ */
+
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", function () {
+  header.classList[this.scrollY > 50 ? "add" : "remove"]("active");
 });
